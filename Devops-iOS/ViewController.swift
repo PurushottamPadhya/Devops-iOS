bfda5b1dabff85e0c9be609a3b9d5c32ae867839
PurushottamPadhya <purushottam.padhya@gmail.com>
2022-02-21 11:49:29 +1100

    Initial commit

---
+++ Devops-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Devops-iOS
+//
+//  Created by Purushottam Padhya on 21/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
