e6fc4c09cdee8a4a70678536c921c9ed31a1030e
PurushottamPadhya <purushottam.padhya@gmail.com>
2022-02-21 12:12:05 +1100

    feature 1 added with inputs

--- Devops-iOS/ViewController.swift
+++ Devops-iOS/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var greetingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        greetingLabel.text = "Hello \(nameTextField.text ?? "")"
+    }
+    
 }
 
